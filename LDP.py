6326532d85597f4511842e57879143cb4a88a507
alisone914 <alisoneill@gmail.com>
2017-05-02 19:04:14 -0400

    Added categories & subcategories

--- LDP.py
+++ LDP.py
@@ -15,6 +15,32 @@ def user():
     user = eventbrite.get_user()
     return user['id']
 
+@app.route('/categories')
+def categories():
+    eventbrite = Eventbrite(token)
+    category_list = eventbrite.get_categories()
+    print(category_list)
+    names = [category['name'] for category in category_list['categories']]
+    print(names)
+    return "test"
+
+@app.route('/subcategories')
+def subcategories():
+    eventbrite = Eventbrite(token)
+    subcategory_list = eventbrite.get_subcategories()
+    print(subcategory_list)
+    subcategory_name = [subcategory['name'] for subcategory in subcategory_list['subcategories']]
+    print(subcategory_name)
+    return '''
+    <html>
+      <head>
+        <title>Intuitive Guest Experiences Platform</title>
+      </head>
+      <body>
+        <h1>subcategory_list</h1>
+      </body>
+    </html>
+    '''
 
 if __name__ == '__main__':
     app.run()
