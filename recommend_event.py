9e8410fa687b5ab9efd05d91cf692b5c9b10b409
Ryan J Ollos <ryanollos@verasonics.com>
2017-05-09 23:05:34 -0700

    Fix retrieving recommendation from EventBrite

--- recommend_event.py
+++ recommend_event.py
@@ -1,8 +1,10 @@
-import logging
-from flask import Flask, render_template
-from flask_ask import Ask, statement, question, session
-from eventbrite import Eventbrite
 import csv
+import logging
+
+from eventbrite import Eventbrite
+from flask import Flask, render_template
+from flask_ask import Ask, question, session, statement
+
 
 app = Flask(__name__)
 ask = Ask(app, "/")
@@ -30,58 +32,60 @@ def new_recommendation():
 
 @ask.intent("DesiredTimeIntent", convert={'desired_time': str})
 def store_desired_time(desired_time):
-    session.attributes['desired_time']= desired_time
+    session.attributes['desired_time'] = desired_time
     checking_msg = render_template('event check', desired_time=desired_time)
     return question(checking_msg)
 
 
-@ask.intent ("SatisfactionIntent", convert={'satisfaction_response': str})
-def generate_recommendations(address, category_input, subcategory_input, location_radius, event_timing):
+@ask.intent("SatisfactionIntent", convert={'satisfaction_response': str})
+def generate_recommendations(satisfaction_response):
+    event = get_recommendation(session.attribute['desired_time'])
+    recommendation_msg = render_template('recommendation', event=event)
+    return question(recommendation_msg)
 
-    #GET CATEGORY ID FROM CATEGORY NAME
+
+def get_recommendation(desired_time):
+    """Get a recommendation from EventBrite."""
     eventbrite = Eventbrite('7BKHCZRFPHVW5PN262TD')
+
+    # GET CATEGORY ID FROM CATEGORY NAME
     address = '181 3rd St, San Francisco, CA 94103'
     category_input = 'Music'
-    subcategory_input = 'Pop'
+    subcategory_input = 'EDM / Electronic'
     location_radius = '1mi'
-    event_timing = store_desired_time
+    event_timing = desired_time
     categoryquery = eventbrite.get_categories()
-    # print(categoryquery)
 
     category_name = [c['name'] for c in categoryquery['categories']]
     category_ids = [c['id'] for c in categoryquery['categories']]
     categories = zip(category_name, category_ids)
 
+    category_id = None
     for x in categories:
         if x[0] == category_input:
             category_id = x[1]
 
-    #GET SUBCATEGORY ID FROM CATEGORY ID
+    # GET SUBCATEGORY ID FROM CATEGORY ID
     subcategoryquery = eventbrite.get_subcategories()
-    # print(categoryquery)
 
     subcategory_name = [c['name'] for c in subcategoryquery['subcategories']]
     subcategory_ids = [c['id'] for c in subcategoryquery['subcategories']]
     subcategories = zip(subcategory_name, subcategory_ids)
 
+    subcategory_id = None
     for x in subcategories:
         if x[0] == subcategory_input:
             subcategory_id = x[1]
 
-    #GET LIST OF EVENTS
-    eventbrite = Eventbrite('tag')
-    eventquery = eventbrite.event_search(**{'location.address': address}, **{'categories': category_id},
-                                         **{'subcategories': subcategory_id}, **{'location.within': location_radius},
-                                         **{'start_date.keyword': event_timing})
-    # print(eventquery)
+    # GET LIST OF EVENTS
+    eventquery = eventbrite.event_search(**{'location.address': address,
+                                            'categories': category_id,
+                                            'subcategories': subcategory_id,
+                                            'location.within': location_radius,
+                                            'start_date.keyword': event_timing})
 
     eventnames = [t['text'] for t in (e['name'] for e in eventquery['events'])]
-    # print(eventnames)
-    event1 = eventnames[0]
-    print(event1)
-
-    recommendation_msg = render_template('recommendation', event1=event1)
-    return question(recommendation_msg)
+    return eventnames[0]
 
     eventdescr = [t['text'] for t in (e['description'] for e in eventquery['events'])]
     # print(eventdescr)
@@ -127,6 +131,7 @@ def generate_recommendations(address, category_input, subcategory_input, locatio
         w = csv.writer(f, delimiter='\t')
         for i in termfile:
             w.writerow(i)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
